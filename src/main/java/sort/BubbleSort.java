a873bdf17d89441b82a685d7eb1b5e1a35f2b8a1
nero <zhanglei-js@secoo.com>
2019-10-12 17:46:11 +0800

    完成快速排序

--- src/main/java/sort/BubbleSort.java
+++ src/main/java/sort/BubbleSort.java
@@ -23,8 +23,14 @@ public class BubbleSort {
 
 
     public static void main(String[] args) {
-        int[] a = new int[]{10,2,11,13,5,7,45,100};
-        bubbleSort(a);
+//        int[] a = new int[]{10,2,11,13,5,7,45,100};
+//        bubbleSort(a);
+
+
+        int[] a = new int[]{2,4,5,1,3,7,6};
+        quickSort(a,0,a.length-1);
+
+
 
         for (int i=0;i<a.length;i++){
             System.out.println(a[i]);
@@ -35,10 +41,15 @@ public class BubbleSort {
 
 
 
-
+    /***
+     *
+     * 冒泡排序
+     * @author Nero
+     * @date 2019-10-10
+     * *@param: arr
+     * @return void
+     */
     private static void bubbleSort(int[] arr){
-
-
         //外层循环，每一次减少最后一个
         for (int i = arr.length ; i > 0 ; i--){
             //内存循环，将当前范围最大的数移到最后
@@ -63,7 +74,49 @@ public class BubbleSort {
 
 
 
+    /***
+     *
+     * 快速排序
+     * @author Nero
+     * @date 2019-10-10
+     * @param: arr
+     * @param: leftIndex
+     * @param: rightIndex
+     * @return void
+     */
+    private static void quickSort(int[] arr,int leftIndex,int rightIndex){
+
+        //-随着每一次递归，left-- 和right ++ ,最后左右两侧的下标都会越界
+        //下标可能会越界，因为每次递归都会进行left-- 和right++,当越界的时候也就比较完了。
+        if (leftIndex < rightIndex){
+
+            int currentL = leftIndex;int currentR = rightIndex;
+            int base = arr[leftIndex];
 
 
+            //如果下标重合，则结束
+            while(currentL < currentR){
+                //- 如果右侧大于左边，则认为有序，则继续往左查找
+                while(currentL < currentR && arr[currentR] > base){
+                    currentR--;
+                }
+                //- 找到比base小的数，则将right设置到left的位置
+                arr[currentL] = arr[currentR];
 
+                //- 从左往右查询，如果小于则认为有序，则继续往右查找
+                while(currentL < currentR && arr[currentL] < base){
+                    currentL++;
+                }
+                //- 找到比base大的数，则将left设置到right的位置
+                arr[currentR] = arr[currentL];
+            }
+
+            //-查找完毕，重合则认为本次查找完毕,将base设置到重合的位置
+            arr[currentL] = base;
+
+
+            quickSort(arr,leftIndex,currentL - 1);
+            quickSort(arr,currentL + 1,rightIndex);
+        }
+    }
 }
